7ec263a3b82c856aac059fbf1de8541f266156da
maykongsn <maykongsn@gmail.com>
2024-08-02 21:22:06 -0300

    test: fix the mock output

--- test/reactsniffer.spec.ts
+++ test/reactsniffer.spec.ts
@@ -1,77 +1,71 @@
 import { describe, expect, test, vi } from 'vitest';
 import { runReactSniffer } from '../lib/reactsniffer';
 
-vi.mock('../lib/reactsniffer', () => ({
-  runReactSniffer: vi.fn(),
-}));
-
 describe("ReactSniffer wrapper", () => {
   test("should return output when runReactSniffer resolves", async () => {
     const mockOutput = { 
       table1: [
         {
           id: '',
-          largeFile: 'Test.tsx',
-          loc: '72',
-          nComponents: '3',
-          nFunctions: '0',
-          nImports: '0'
+          "LOC": 72,
+          "Large File": "Test.tsx",
+          N_Components: 3,
+          N_Functions: 0,
+          N_Imports: 0
         }
       ],
       table2: [
         {
-          file: 'Button.tsx',
-          component: 'Button',
-          lc: 'Y',
-          tp: 'N',
-          iic: 'N',
-          fu: '0',
-          dom: '0',
-          jsx: '0',
-          uc: '0',
-          pis: '0',
+          File: 'Button.tsx',
+          Component: 'Button',
+          LC: 'Y',
+          TP: 'N',
+          IIC: 'N',
+          FU: 0,
+          DOM: 0,
+          JSX: 0,
+          UC: 0,
+          PIS: 0,
         },
         {
-          file: 'Test.tsx',
-          component: 'ListNames',
-          lc: 'N',
-          tp: 'N',
-          iic: 'N',
-          fu: '0',
-          dom: '0',
-          jsx: '0',
-          uc: '0',
-          pis: '1',
+          File: 'Test.tsx',
+          Component: 'ListNames',
+          LC: 'N',
+          TP: 'N',
+          IIC: 'N',
+          FU: 0,
+          DOM: 0,
+          JSX: 0,
+          UC: 0,
+          PIS: 1,
         },
         {
-          file: 'Test.tsx',
-          component: 'ListNamesB',
-          lc: 'Y',
-          tp: 'N',
-          iic: 'N',
-          fu: '0',
-          dom: '0',
-          jsx: '0',
-          uc: '0',
-          pis: '1',
+          File: 'Test.tsx',
+          Component: 'ListNamesB',
+          LC: 'N',
+          TP: 'N',
+          IIC: 'N',
+          FU: 0,
+          DOM: 0,
+          JSX: 0,
+          UC: 0,
+          PIS: 1,
         },
         {
-          file: 'Test.tsx',
-          component: 'ListNamesC',
-          lc: 'Y',
-          tp: 'N',
-          iic: 'N',
-          fu: '0',
-          dom: '0',
-          jsx: '0',
-          uc: '0',
-          pis: '1',
+          File: 'Test.tsx',
+          Component: 'ListNamesC',
+          LC: 'N',
+          TP: 'N',
+          IIC: 'N',
+          FU: 0,
+          DOM: 0,
+          JSX: 0,
+          UC: 0,
+          PIS: 1,
         }
       ] 
     };
 
-    vi.mocked(runReactSniffer).mockResolvedValue(mockOutput);
-    
-    expect(await runReactSniffer('/docs/code-examples')).toEqual(mockOutput);
+    expect(await runReactSniffer('test/components')).toEqual(mockOutput);
   });
 });
