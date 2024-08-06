69ebbccfac393e43171efcb46cfc4e4a29f5118e
maykongsn <maykongsn@gmail.com>
2024-08-06 11:42:26 -0300

    test: file reader output

---
+++ test/mocks/code.ts
@@ -0,0 +1,294 @@
+export const mockCode =
+  [
+    {
+      path: 'test/components/Button.tsx',
+      content: 'import React from "react";\n' +
+        '\n' +
+        'const Button = () => {\n' +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        "  console.log('test')\n" +
+        '\n' +
+        '  return (\n' +
+        '    <><p>Test</p></>\n' +
+        '  );\n' +
+        '}'
+    },
+    {
+      path: 'test/components/Test.tsx',
+      content: 'import React from "react";\n' +
+        '\n' +
+        'class ListNames extends React.Component {\n' +
+        '  constructor(props){\n' +
+        '    super(props);\n' +
+        '    this.state = {\n' +
+        '      names: props.names,\n' +
+        '      ages: props.ages,\n' +
+        '    };\n' +
+        '  }\n' +
+        '  render() {\n' +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        "    console.log('test')\n" +
+        '  }\n' +
+        '}\n' +
+        '\n' +
+        'class ListNamesB extends React.Component {\n' +
+        '  constructor(props){\n' +
+        '    super(props);\n' +
+        '    this.state = {\n' +
+        '      names: props.names,\n' +
+        '      ages: props.ages,\n' +
+        '    };\n' +
+        '  }\n' +
+        '}\n' +
+        '\n' +
+        'class ListNamesC extends React.Component {\n' +
+        '  constructor(props){\n' +
+        '    super(props);\n' +
+        '    this.state = {\n' +
+        '      names: props.names,\n' +
+        '      ages: props.ages,\n' +
+        '    };\n' +
+        '  }\n' +
+        '}'
+    }
+  ]
+
