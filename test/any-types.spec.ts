8ae5ec1f6098278948bf5473575585eaffd17242
maykongsn <maykongsn@gmail.com>
2024-08-13 14:14:05 -0300

    fix: typos in any type test

--- test/any-types.spec.ts
+++ test/any-types.spec.ts
@@ -1,10 +1,10 @@
 import { describe, expect, test } from "vitest";
 import { parseAST } from "../lib/utils/parser";
-import { mockAnyType } from "./mocks/any-type-mock";
+import { mockAnyType } from "./mocks/code-smells-mock";
 import { anyType } from "../lib/smells/any-type";
 
 describe("Any type", () => {
-  test("should return tree occurrences of any type", () => {
+  test("should return three occurrences of Any Type", () => {
     const expectedOutput = [
       { start: 3, end: 3, filename: 'test/components/Any.tsx' },
       { start: 4, end: 4, filename: 'test/components/Any.tsx' },
