f6e593fa2d26e9ba3232dcde732e0cd0814b0a7f
maykongsn <maykongsn@gmail.com>
2024-08-21 08:53:18 -0300

    feat: detection of multiple booleans for state

---
+++ lib/smells/multiple-booleans-for-state.ts
@@ -0,0 +1,27 @@
+import { ParseResult } from "@babel/parser";
+import traverse from "@babel/traverse";
+import { File, isBooleanLiteral } from "@babel/types";
+import { SourceLocation } from "../types";
+
+export const multipleBooleansForState = (ast: ParseResult<File>) => {
+  const states: SourceLocation[] = [];
+
+  traverse(ast, {
+    CallExpression(path) {
+      if (
+        path.node.callee.type === "Identifier" || path.node.callee.type === "MemberExpression" &&
+        path.node.callee.name === "useState" &&
+        path.node.arguments.length > 0 &&
+        isBooleanLiteral(path.node.arguments[0])
+      ) {
+        states.push({
+          start: path.node.callee.loc?.start.line,
+          end: path.node.callee.loc?.end.line,
+          filename: path.node.callee.loc?.filename
+        });
+      }
+    }
+  });
+
+  return states.length > 4 ? states : []
+}
