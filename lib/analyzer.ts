fb5ea34e5ce9210d4c9a9ef1e6fae42bba02fcf4
maykongsn <maykongsn@gmail.com>
2024-09-18 09:59:39 -0300

    refactor: analysis output using mapped types

--- lib/analyzer.ts
+++ lib/analyzer.ts
@@ -1,3 +1,5 @@
+import { ParseResult } from "@babel/parser";
+import { File } from "@babel/types";
 import { readFiles } from "./utils/file-reader";
 import { parseAST } from "./utils/parser";
 import { SourceLocation, TSXFile } from "./types";
@@ -10,34 +12,44 @@ import { nonNullAssertions } from "./smells/non-null-assertions";
 import { overlyFlexibleProps } from "./smells/overly-flexible-props";
 
 export type AnalysisOutput = {
-  anyType?: SourceLocation[];
-  nonNullAssertions?: SourceLocation[];
-  missingUnionTypeAbstraction?: SourceLocation[];
-  enumImplicitValues?: SourceLocation[];
-  multipleBooleansForState?: SourceLocation[];
-  overlyFlexibleProps?: SourceLocation[];
+  [key: string]: {
+    [K in keyof Analyzers]?: ReturnType<Analyzers[K]>;
+  }
 }
 
-const analyzers = {
-  anyType,
-  nonNullAssertions,
-  missingUnionTypeAbstraction,
-  enumImplicitValues,
-  multipleBooleansForState,
-  overlyFlexibleProps
+export type AnalyzerFunction<T> = (ast: ParseResult<File>) => T;
+
+type Analyzers = {
+  anyType: AnalyzerFunction<SourceLocation[]>;
+  nonNullAssertions: AnalyzerFunction<SourceLocation[]>;
+  missingUnionTypeAbstraction: AnalyzerFunction<SourceLocation[]>;
+  enumImplicitValues: AnalyzerFunction<SourceLocation[]>;
+  multipleBooleansForState: AnalyzerFunction<SourceLocation[]>;
+  overlyFlexibleProps: AnalyzerFunction<SourceLocation[]>
 }
 
 export const analyzeFile = (file: TSXFile): AnalysisOutput => {
   const ast = parseAST(file);
+  
+  const analyzers: Analyzers = {
+    anyType,
+    enumImplicitValues,
+    missingUnionTypeAbstraction,
+    multipleBooleansForState,
+    nonNullAssertions,
+    overlyFlexibleProps
+  };
 
-  return Object.fromEntries(
-    Object.entries(analyzers).map(([key, analyzer]) => [key, analyzer(ast)])
-  )
+  return {
+    [file.path]: Object.fromEntries(
+      Object.entries(analyzers).map(([key, analyzer]) => [key, analyzer(ast)])
+    )
+  }
 }
 
 export const analyze = async (path: string) => {
   const analysis: AnalysisOutput[] = [];
-
+  
   for await (const file of readFiles(path)) {
     analysis.push(analyzeFile(file));
   }
